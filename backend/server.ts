e5a99cf8614a6d4f4cc7ae224b3c19bfd5d41215
Joseph Maramba <josephmaramba50@gmail.com>
2023-04-28 12:55:39 +0300

    setup express server

---
+++ backend/server.ts
@@ -0,0 +1,17 @@
+
+import express, { Express, Response } from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app: Express = express();
+// eslint-disable-next-line no-undef
+const PORT = process.env.PORT || 5500;
+
+app.get('/', (_, res: Response) => {
+    res.status(200).json({ message: 'Hello, world!' });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server listening on port: ${PORT}`);
+});
